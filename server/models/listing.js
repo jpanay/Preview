9a222ef6748300894860cfee84839ca37aa12b92
Jason Panay <jason@Jasons-MacBook-Pro.local>
2021-10-15 20:31:17 -0400

    updated schema

--- server/models/listing.js
+++ server/models/listing.js
@@ -2,14 +2,19 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const listingSchema = new Schema({
-  location: String,
   manager: String,
+  location: String,
+  area: String,
+  zipcode: String,
   position: Object,
   unit: String,
   price: Number,
   beds: Number,
   baths: Number,
   description: String,
+  occupied: Boolean,
+  previewed: Boolean,
+  listed: Boolean
 });
 
 const Listing = mongoose.model('Listing', listingSchema);
