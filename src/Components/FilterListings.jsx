843ace028aa5c7209814a9f5fcc4d605c81878ef
Jason Panay <jason@Jasons-MacBook-Pro.local>
2021-10-17 17:31:56 -0400

    Setup filter listings and small changes

---
+++ src/Components/FilterListings.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+class FilterListings extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <div>FILTER</div>
+      </div>
+    );
+  }
+}
+
+export default FilterListings;
--- src/Components/Listings.jsx
+++ src/Components/Listings.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import Listing from "./Listing";
 import AddListing from "./AddListing";
+import FilterListings from "./FilterListings";
 
 let Listings = ({ listings, selectListing }) => {
   return (
     <div id="listings-wrapper">
-      <div className="listing-controls">
-        <div className="listing-control">Edit</div>
-        <div className="listing-control">Add</div>
-      </div>
-
+      <FilterListings />
       <AddListing />
       
       <div id="listings" className="listings">
@@ -19,6 +16,9 @@ let Listings = ({ listings, selectListing }) => {
           <div className="price">Price</div>
           <div className="beds">Bed</div>
           <div className="baths">Bath</div>
+          <div className="">Occupied</div>
+          <div className="">Previewed</div>
+          <div className="">Listed</div>
         </div>
 
         {listings.map(function (listing, i) {
