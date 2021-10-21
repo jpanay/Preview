0bd14976b6c1e64a12151e95cc4344314e86ebc8
AntonMenchaca <menchacanton@gmail.com>
2021-10-21 00:15:54 -0400

    fix filtering functionality

--- src/Components/FilterListings.jsx
+++ src/Components/FilterListings.jsx
@@ -4,54 +4,69 @@ class FilterListings extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      filterOccupied: false,
-      filterPreviewed: false,
-      filterListed: false,
+      occupied: false,
+      previewed: false,
+      listed: false,
     };
 
     this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
     this.handleFilterListings = this.handleFilterListings.bind(this);
   }
 
-  handleFilterListings() {
+  handleFilterListings(e) {
+    console.log(e.target.name);
     if (
-      this.state.filterListed === false &&
-      this.state.filterPreviewed === false &&
-      this.state.filterOccupied === false
+      this.state.listed === false &&
+      this.state.previewed === false &&
+      this.state.occupied === false
     ) {
       //dispatch an action that changes the filter list array to be a blank array which would render all listings
       this.props.filterListings([]);
     } else {
       //create a filtered listing that matches the exact values
-      let filteredList = this.props.listings.filter((listing) => {
-        if (
-          listing.occupied === this.state.filterOccupied &&
-          listing.previewed === this.state.filterPreviewed &&
-          listing.listed === this.state.filterListed
-        ) {
-          return listing;
+      if (
+        (this.state.occupied && this.state.previewed) ||
+        (this.state.occupied && this.state.listed) ||
+        (this.state.previewed && this.state.listed)
+      ) {
+        let filteredList = this.props.listings.filter((listing) => {
+          if (
+            listing.occupied === this.state.occupied &&
+            listing.previewed === this.state.previewed &&
+            listing.listed === this.state.listed
+          ) {
+            return listing;
+          }
+        });
+        if (filteredList.length === 0) {
+          this.props.filterListings([
+            {
+              address: "no matching filters",
+              area: "n/a",
+              baths: 0,
+              beds: 0,
+              description: "n/a",
+              full_address: "n/a",
+              listed: false,
+              manager: "n/a",
+              occupied: false,
+              position: { lat: 40.7385431, lng: -73.9819469 },
+              previewed: false,
+              price: 0,
+              unit: "n/a",
+              zipcode: "n/a",
+            },
+          ]);
+        } else {
+          this.props.filterListings(filteredList);
         }
-      });
-      if (filteredList.length === 0) {
-        this.props.filterListings([
-          {
-            address: "no matching filters",
-            area: "n/a",
-            baths: 0,
-            beds: 0,
-            description: "n/a",
-            full_address: "n/a",
-            listed: false,
-            manager: "n/a",
-            occupied: false,
-            position: { lat: 40.7385431, lng: -73.9819469 },
-            previewed: false,
-            price: 0,
-            unit: "n/a",
-            zipcode: "n/a",
-          },
-        ]);
       } else {
+        let filteredList = this.props.listings.filter((listing) => {
+          console.log(listing[e.target.name]);
+          if (listing[e.target.name] === this.state[e.target.name]) {
+            return listing;
+          }
+        });
         this.props.filterListings(filteredList);
       }
     }
@@ -60,7 +75,7 @@ class FilterListings extends React.Component {
   handleCheckboxChange(e) {
     console.log(!this.state[event.target.name]);
     this.setState({ [e.target.name]: !this.state[event.target.name] }, () =>
-      this.handleFilterListings()
+      this.handleFilterListings(e)
     );
   }
 
@@ -71,9 +86,9 @@ class FilterListings extends React.Component {
           <input
             className="form-check-input"
             type="checkbox"
-            name="filterOccupied"
+            name="occupied"
             id="filter-occupied"
-            defaultChecked={this.state.filterOccupied}
+            defaultChecked={this.state.occupied}
             onChange={this.handleCheckboxChange}
           />
           <label className="form-check-label" htmlFor="filter-occupied">
@@ -84,9 +99,9 @@ class FilterListings extends React.Component {
           <input
             className="form-check-input"
             type="checkbox"
-            name="filterPreviewed"
+            name="previewed"
             id="filter-previewed"
-            defaultChecked={this.state.filterPreviewed}
+            defaultChecked={this.state.previewed}
             onChange={this.handleCheckboxChange}
           />
           <label className="form-check-label" htmlFor="filter-previewed">
@@ -97,9 +112,9 @@ class FilterListings extends React.Component {
           <input
             className="form-check-input"
             type="checkbox"
-            name="filterListed"
+            name="listed"
             id="filter-listed"
-            defaultChecked={this.state.filterListed}
+            defaultChecked={this.state.listed}
             onChange={this.handleCheckboxChange}
           />
           <label className="form-check-label" htmlFor="filter-listed">
